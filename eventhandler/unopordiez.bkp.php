139f6b9d2b5c1aba483567e7b2e09e9a91e4a6f2
Erick Birbe <erickcion@gmail.com>
2015-11-27 02:10:38 -0430

    Mejoras al sistema de respuestas - Menos mensajes de respuesta, las respuestas ahora se dan en un solo msj por tipo de respuesta - Capacidad de solicitar el total de datos enviados - Filtro completo de todo aquello que no sea numero - Muestra en mensaje entrante sin filtros en la consola

--- eventhandler/unopordiez.bkp.php
+++ eventhandler/unopordiez.bkp.php
@@ -17,35 +17,18 @@ class EBTMsgReceived extends EBTMessage
 		$sClientNumber = $this->get_header('From');
 		$sDateSent = $this->get_header('Sent');
 		$sMsgIn = trim($this->get_message());
+		$sMsgIn = str_replace(',', ' ', $sMsgIn);
 		$sMsgOut = "";
-		$sOldRes = "";
-		$aResp = array(
-			'CI_NOVAL' => "",
-			'CI_NOREG' => "",
-			'CI_ERROR' => "",
-		);
 
-		if (strlen($sClientNumber) < 6)
-		{
-			echo 'ERROR: Numero de telefono muy corto (' . $sClientNumber . ')';
-			return;
-		}
-
-		echo "--- PHONE: " . $sClientNumber . "---\n";
-		echo $sMsgIn . "\n";
-		echo "--- END SMS ---\n";
-
-		if (strtoupper($sMsgIn) == 'TOTAL')
-		{
-			$sMsgIn = '0000001';
-		}
-
-		$sMsgIn = preg_replace('/[^0-9]/', ' ', $sMsgIn);
 		$aCedulas = split(' ', $sMsgIn);
 		foreach ($aCedulas as $sCedula)
 		{
 			// Sanitize
+			$sCedula = str_replace(',', '', $sCedula);
+			$sCedula = str_replace('.', '', $sCedula);
 			$sCedula =  trim($sCedula);
+
+			echo "Cedula: " . $sCedula . "\n";
 
 			// Si el mensaje esta en blanco es solo texto no hace nada
 			if (empty($sCedula) or !is_numeric($sCedula))
@@ -54,9 +37,9 @@ class EBTMsgReceived extends EBTMessage
 			}
 
 			// Si la cedula esta fuera de rango
-			if(strlen($sCedula) < 5 or strlen($sCedula) > 8)
+			if(strlen($sCedula) < 6 or strlen($sCedula) > 8)
 			{
-				$aResp['CI_NOVAL'] .= $sCedula . "\n";
+				$sMsgOut = $sCedula . " No parece ser un numero de cedula.";
 			}
 			else
 			{
@@ -69,6 +52,10 @@ class EBTMsgReceived extends EBTMessage
 				$data_string = json_encode($param);
 				$ch = curl_init($this->validation_server);
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				// PROXY
+				curl_setopt($ch, CURLOPT_PROXY, "172.16.2.8");
+				curl_setopt($ch, CURLOPT_PROXYPORT, "3128");
+				curl_setopt($ch, CURLOPT_PROXYTYPE, "HTTP");
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 				curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -76,72 +63,33 @@ class EBTMsgReceived extends EBTMessage
 						'Content-Length: ' . strlen($data_string)
 					)
 				);
-				// PROXY
-				/*
-				curl_setopt($ch, CURLOPT_PROXY, "172.16.2.8");
-				curl_setopt($ch, CURLOPT_PROXYPORT, "3128");
-				curl_setopt($ch, CURLOPT_PROXYTYPE, "HTTP");
-				*/
 
 				$result = curl_exec($ch);
 				$jData = json_decode($result);
 				var_dump($result);
-				// var_dump($jData);
+				var_dump($jData);
 
 				if ($jData !== NULL)
 				{
-					if ($jData->existe == 1)
+					if ($jData->existe)
 					{
 						// $sMsgOut = "Se ha registrado " . $jData->cedula . " con exito.";
 						continue;
 					}
 					else
 					{
-						if($jData->existe == 'MSJ')
-						{
-							$aResp['MSJ'] = $jData->mensaje;
-						}
-						// $aResp['CI_NOREG'] .= $sCedula . "\n";
+						$sMsgOut = "La cedula " . $jData->cedula . " no está registrada.";
 						continue;
 					}
 				}
 				else
 				{
-					if($sOldRes == $result)
-					{
-						$result = NULL;
-					}
-					$aResp['CI_ERROR'] .= $result . "\n" . $sCedula;
-					$sOldRes = $result;
+					$sMsgOut = "Ocurrio un error con " . $sCedula . " - (" . $result . ")";
 				}
 			}
-		}
 
-		foreach($aResp as $key => $value)
-		{
-			$sMsgOut = "";
-			if ($value  != "")
+			if ($sMsgOut  != "")
 			{
-				switch ($key)
-				{
-					case 'CI_NOVAL':
-						$sMsgOut = "(Utilice solo numeros y espacios)\n";
-						$sMsgOut .= "NO ES UNA CEDULA:\n";
-						break;
-					case 'CI_NOREG':
-						$sMsgOut = "NO ESTA REGISTRADA:\n";
-						break;
-					case 'CI_ERROR':
-						$sMsgOut = "ERROR DE TRANSMISION:\n";
-						break;
-					case 'MSJ':
-						$sMsgOut = "MENSAJE:\n";
-						break;
-					default:
-						break;
-				}
-				$sMsgOut .= $value;
-
 				// Send the automatic response
 				$oResponse = new EBTMessage();
 				$oResponse->set_header('To', $sClientNumber);
@@ -149,6 +97,5 @@ class EBTMsgReceived extends EBTMessage
 				$oResponse->write();
 			}
 		}
-
 	}
 }
