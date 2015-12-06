2331b56930ca7827c17933ef04f155a651a45f8d
Erick Birbe <erickcion@gmail.com>
2015-12-06 07:27:39 -0430

    Showing TLF

--- eventhandler/unopordiez.php
+++ eventhandler/unopordiez.php
@@ -83,7 +83,7 @@ class EBTMsgReceived extends EBTMessage
 
 				$result = curl_exec($ch);
 				$jData = json_decode($result);
-				var_dump('PARA: ' . $sClientNumber .' '.$result);
+				var_dump('ENC:' . $sClientNumber .' '.$result);
 				// var_dump($jData);
 
 				if ($jData !== NULL)
